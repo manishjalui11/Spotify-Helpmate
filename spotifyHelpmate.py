51610f7cb99a450a99f7d4e58ac39e0b1772fe45
Manish <manishjalui11@gmail.com>
2021-06-10 00:14:09 +0530

    SH v0.1.1 base spotify api python created

--- spotifyHelpmate.py
+++ spotifyHelpmate.py
@@ -1,4 +1,8 @@
-
+from Backend.spotifyClient import SpotifyAPI
+from Config.config import *
+import json
 
 class SpotifyHelpmate():
-    print("spotifyHelpmate")
+    spotify =  SpotifyAPI(clientId, clientSecret)
+    # print(spotify.search({"track": "breathe"}))
+    print(json.dumps(spotify.search({"track": "breathe"}), indent=4, sort_keys=True))
