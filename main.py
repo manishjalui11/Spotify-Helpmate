d45473f09d3ba11876e68116f23dfb8f0859e495
Manish <manishjalui11@gmail.com>
2021-06-15 13:54:45 +0530

    SH v0.1.4
    search track optimised into fuctions and classes.
    Also search is more robost

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from spotifyHelpmate import SpotifyHelpmate
 
 def main():
     SpotifyHelpmate()
-
 if __name__=='__main__':
     main()
--- spotifyHelpmate.py
+++ spotifyHelpmate.py
@@ -1,45 +1,71 @@
 from Backend.spotifyClient import SpotifyAPI
 from Config.config import *
-import json
 
 class SpotifyHelpmate():
-    spotify =  SpotifyAPI(clientId, clientSecret)
-    print("breathe")
-    tracks = []
-    infos = spotify.search({"track": "Free Spirit"})["tracks"]["items"]
-    for info in infos:  
-        trackName = info["name"]
-        trackUrl = info["external_urls"]['spotify']
-        trackId = info["id"]
-        album = info["album"]
-        albumName = album['name']
-        albumId = album['id']
-        albumCover = album['images'][0]['url']
-        albumUrl = album['external_urls']['spotify']
-        artistsInfo = info["artists"]
-        artists = []
-        for artistInfo in artistsInfo:
-            artistName = artistInfo['name']
-            artistId = artistInfo['id']
-            artistUrl = artistInfo['external_urls']['spotify']
-            artist = {
-                'artistname': artistName,
-                'artistid': artistId,
-                'artisturl': artistUrl}
-            artists.append(artist)
-        # artist.clear()
-        track = {
-                'trackname': trackName,
-                'trackid': trackId,
-                'trackurl': trackUrl,
-                'albumname': albumName,
-                'albumid': albumId,
-                'albumurl': albumUrl,
-                'albumcover': albumCover,
-                'artist': artists}
-        tracks.append(track)
-        # print(track)
-        # print("\n\n #### \\n\n")
-        # track.clear()
+    def __init__(self):
+        self.spotify =  SpotifyAPI(clientId, clientSecret)
 
-    print(tracks)
+    def searchTrack(self, songName):
+        tracks = []
+        infos = self.spotify.search({"track": songName})["tracks"]["items"]
+        for info in infos:  
+            trackName = info["name"]
+            trackUrl = info["external_urls"]['spotify']
+            trackId = info["id"]
+            album = info["album"]
+            albumName = album['name']
+            albumId = album['id']
+            albumCover = album['images'][0]['url']
+            albumUrl = album['external_urls']['spotify']
+            artistsInfo = info["artists"]
+            artists = []
+            for artistInfo in artistsInfo:
+                artistName = artistInfo['name']
+                artistId = artistInfo['id']
+                artistUrl = artistInfo['external_urls']['spotify']
+                artist = {
+                    'artistname': artistName,
+                    'artistid': artistId,
+                    'artisturl': artistUrl}
+                artists.append(artist)
+            track = {
+                    'trackname': trackName,
+                    'trackid': trackId,
+                    'trackurl': trackUrl,
+                    'albumname': albumName,
+                    'albumid': albumId,
+                    'albumurl': albumUrl,
+                    'albumcover': albumCover,
+                    'artist': artists}
+            tracks.append(track)
+        return tracks
+
+    def getTrackName(self, track):
+        return track['trackname']
+
+    def getTrackId(self, track):
+        return track['trackid']
+
+    def getTrackUrl(self, track):
+        return track['trackurl']
+
+    def getAlbumName(self, track):
+        return track['albumname']
+
+    def getAlbumId(self, track):
+        return track['albumid']
+
+    def getAlbumUrl(self, track):
+        return track['albumurl']
+
+    def getAlbumCover(self, track):
+        return track['albumcover']
+
+    def getArtistName(self, track):
+        return ', '.join(artist['artistname'] for artist in track['artist'])
+
+    def getArtistId(self, track):
+        return ', '.join(artist['artistid'] for artist in track['artist'])
+
+    def getArtistUrl(self, track):
+        return ', '.join(artist['artisturl'] for artist in track['artist'])
